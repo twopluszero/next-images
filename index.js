581b3afbbef323529b1e022a06810f887164f292
Aref Aslani <arefaslani@gmail.com>
2018-04-17 08:46:59 +0430

    use same images on the server and client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = (nextConfig = {}) => {
   return Object.assign({}, nextConfig, {
     webpack(config, options) {
+      const { isServer } = options;
       if (!options.defaultLoaders) {
         throw new Error(
           'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
@@ -19,7 +20,7 @@ module.exports = (nextConfig = {}) => {
               limit,
               fallback: "file-loader",
               publicPath: `${assetPrefix}/_next/static/images/`,
-              outputPath: "static/images/",
+              outputPath: `${isServer ? "../" : ""}static/images/`,
               name: "[name]-[hash].[ext]"
             }
           }
