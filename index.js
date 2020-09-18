cebcd24fd250b839577b95f4f3158a0085c96228
Aref Aslani <arefaslani@gmail.com>
2020-09-18 13:31:03 +0430

    Fix a bug with file extensions option

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports = (nextConfig = {}) => {
       }
 
       config.module.rules.push({
-        test: new RegExp(`\.(${fileExtensions.join('|')})$`),
+        test: new RegExp(`\.(${nextConfig.fileExtensions.join('|')})$`),
         // Next.js already handles url() in css/sass/scss files
         issuer: /\.\w+(?<!(s?c|sa)ss)$/i,
         exclude: nextConfig.exclude,
