209041aab5082f7cbbc9b0abba0cdf80140f18c7
lvancraen <louis@transit.app>
2021-11-01 16:08:01 -0400

    fix: invalid group specifier name in safari

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ module.exports = ({ dynamicAssetPrefix = false, ...nextConfig } = {}) => {
       config.module.rules.push({
         test: new RegExp(`\\.(${nextConfig.fileExtensions.join('|')})$`),
         // Next.js already handles url() in css/sass/scss files
-        issuer: /\.\w+(?<!(s?c|sa)ss)$/i,
+        issuer: new RegExp('\.\w+(?<!(s?c|sa)ss)$', 'i'),
         exclude: nextConfig.exclude,
         use: [
           {
